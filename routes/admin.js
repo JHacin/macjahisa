d574941ed8e25c93f13c24f203400c029b1f9eb1
Jan Hacin <jan.hacin@gmail.com>
2018-01-18 20:07:00 +0100

    init custom cms template

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,13 @@
 var express             = require("express");
 var router              = express.Router({mergeParams: true});
 
+router.get("/", function(req, res){
+  res.redirect("/admin/muce");
+});
 
+router.get("/muce", function(req, res){
+  res.render("admin/muce");
+});
 
 
 
