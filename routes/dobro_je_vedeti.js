9fc479f31cd735f3a7b77e5005c69e4fbb1ec331
Jan Hacin <jan.hacin@gmail.com>
2018-02-04 18:13:50 +0100

    add edu content show page

--- routes/dobro_je_vedeti.js
+++ routes/dobro_je_vedeti.js
@@ -2,14 +2,17 @@ var express = require("express");
 var router = express.Router({mergeParams: true});
 var Podstran = require("../models/podstran");
 var Clanek = require("../models/clanek");
+var Izobrazevalna_vsebina = require("../models/izobrazevalna_vsebina");
 
 router.get("/izobrazevalne_vsebine", function(req, res){
   Podstran.findOne({naslov: "Izobraževalne vsebine"}, function(err, podstran){
     if(err) return console.log(err);
-    res.render("dobro_je_vedeti/izobrazevalne_vsebine", {nav_kategorije: req.nav_kategorije,
-    nav_podstrani: req.nav_podstrani, sidebar_novice: req.sidebar_novice,
-    sidebar_muce: req.sidebar_muce, title: "Izobraževalne vsebine | Mačja hiša",
-    podstran: podstran})
+    Izobrazevalna_vsebina.find({}).sort({datum: -1}).exec(function(err, vsebine){
+      res.render("dobro_je_vedeti/izobrazevalne_vsebine", {nav_kategorije: req.nav_kategorije,
+      nav_podstrani: req.nav_podstrani, sidebar_novice: req.sidebar_novice,
+      sidebar_muce: req.sidebar_muce, title: "Izobraževalne vsebine | Mačja hiša",
+      podstran: podstran, vsebine: vsebine})
+    });
   });
 });
 
