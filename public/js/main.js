6965a8a5a917e5c25cd1887ad87954cf4b14c4db
Jan Hacin <jan.hacin@gmail.com>
2018-02-05 20:21:11 +0100

    add rehomed cats show page

--- public/js/main.js
+++ public/js/main.js
@@ -26,6 +26,19 @@ $(document).ready( function() {
     show_paginate_if_one: false
   });
 
+  // pagination - MUCE
+  $('#v_novem_domu_container').pajinate({
+    items_per_page : 10,
+    item_container_id : '.content',
+    nav_panel_id : '.page_navigation',
+    num_page_links_to_display: 5,
+    nav_label_prev: "Nazaj",
+    nav_label_next: "Naprej",
+    nav_label_first: "Na začetek",
+    nav_label_last: "Na konec",
+    show_paginate_if_one: false
+  });
+
   // dropdown menu
   $(".stellarnav").stellarNav({
     position: "left",
--- routes/v_novem_domu.js
+++ routes/v_novem_domu.js
@@ -1,21 +1,21 @@
 var express             = require("express");
 var router              = express.Router({mergeParams: true});
 var Podstran = require("../models/podstran");
+var Muca = require("../models/muca");
 
 router.get("/", function(req, res){
-  res.render("v_novem_domu/index");
-});
-
-router.get("/:podstran", function(req, res){
-  Podstran.findOne({url: req.params.podstran}, function(err, podstran){
-    res.render("posvojitev/show",
-    {
-      podstran: podstran,
-      nav_kategorije: req.nav_kategorije,
-      nav_podstrani: req.nav_podstrani,
-      sidebar_novice: req.sidebar_novice,
-      sidebar_muce: req.sidebar_muce,
-      title: podstran.naslov + " | Mačja hiša"
+  Podstran.findOne({url: "v_novem_domu"}, function(err, podstran){
+    Muca.find().where("status").equals(4).sort({datum: -1}).exec(function(err, muce){
+      res.render("v_novem_domu/index",
+      {
+        muce: muce,
+        podstran: podstran,
+        nav_kategorije: req.nav_kategorije,
+        nav_podstrani: req.nav_podstrani,
+        sidebar_novice: req.sidebar_novice,
+        sidebar_muce: req.sidebar_muce,
+        title: podstran.naslov + " | Mačja hiša"
+      });
     });
   });
 });
