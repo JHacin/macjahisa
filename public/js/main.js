3e04aebdce58fd9a4d38ce0187a60ad684322733
Jan Hacin <jan.hacin@gmail.com>
2018-02-09 20:37:52 +0100

    add filter, search and pagination to cats list

--- public/js/main.js
+++ public/js/main.js
@@ -13,23 +13,17 @@ $(document).ready( function() {
     show_paginate_if_one: false
   });
 
-  // pagination - MUCE
-  $('#muce_container').pajinate({
-    items_per_page : 9,
-    item_container_id : '.content',
-    nav_panel_id : '.page_navigation',
-    num_page_links_to_display: 5,
-    nav_label_prev: "Nazaj",
-    nav_label_next: "Naprej",
-    nav_label_first: "Na začetek",
-    nav_label_last: "Na konec",
-    show_paginate_if_one: false
-  });
-
   // dropdown menu
   $(".stellarnav").stellarNav({
     position: "left",
     breakpoint: 600
   });
+
+  // MUCE LIST
+  $('.muce_seznam').jplist({
+              itemsBox: '.list',
+              itemPath: '.list-item',
+              panelPath: '.jplist-panel'
+           });
 
 });
