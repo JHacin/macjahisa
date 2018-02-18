5ac25b5cf2cdfd346fbfc04a764273261b868070
Jan Hacin <jan.hacin@gmail.com>
2018-02-18 16:13:44 +0100

    remake news pagination with jplist

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var novice = require("./routes/novice.js");
 var v_novem_domu = require("./routes/v_novem_domu.js");
 
 // CONFIG
-mongoose.connect("mongodb://localhost/macjahisa" || process.env.DATABASE, {useMongoClient: true});
+mongoose.connect("mongodb://localhost/macjahisa" || process.env.DATABASE);
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,5 @@
 $(document).ready( function() {
 
-  // pagination - NOVICE
-  $('#novice_container').pajinate({
-    items_per_page : 10,
-    item_container_id : '.content',
-    nav_panel_id : '.page_navigation',
-    num_page_links_to_display: 5,
-    nav_label_prev: "Nazaj",
-    nav_label_next: "Naprej",
-    nav_label_first: "Na začetek",
-    nav_label_last: "Na konec",
-    show_paginate_if_one: false
-  });
-
   // dropdown menu
   $(".stellarnav").stellarNav({
     position: "left",
@@ -25,6 +12,13 @@ $(document).ready( function() {
     itemPath: '.list-item',
     panelPath: '.jplist-panel'
    });
+
+   // NOVICE LIST
+   $('.novice_seznam').jplist({
+     itemsBox: '.list',
+     itemPath: '.list-item',
+     panelPath: '.jplist-panel'
+    });
 
    // MUCA IMAGE SLIDER
    $('.photos').slick({
