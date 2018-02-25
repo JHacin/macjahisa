7dc953c1a8be6eb1eb7ce7d3b2ad40e83967d766
Jan Hacin <jan.hacin@gmail.com>
2018-02-25 19:18:03 +0100

    add paypal images and scroll to top button

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready( function() {
 
+  // check if window is top - if not, display nav_button
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100) {
+      $(".scrollToTop").fadeIn();
+    } else {
+      $(".scrollToTop").fadeOut();
+    }
+  });
+
+  // scroll to top on btn click
+  $(".scrollToTop").click(function(){
+    $("html, body").animate({scrollTop: 0}, 600);
+    return false;
+  });
+
   // dropdown menu
   $(".stellarnav").stellarNav({
     position: "left",
