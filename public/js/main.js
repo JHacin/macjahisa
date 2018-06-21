770ab398f0062fc39d0645fc76505417b02e03e0
Jan Hacin <jan.hacin@gmail.com>
2018-06-21 16:28:01 +0200

    disable hover effects on mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,41 @@
 $(document).ready( function() {
 
+  // Disable hover effects on mobile
+  function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.body;
+    var lastTouchTime = 0;
+
+    function enableHover() {
+        // filter emulated events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
+
+
   // SAFARI TEMPORARY FIX: prevents donate button from showing above mobile navbar
   $(window).on('resize', function(){
       var win = $(this); //this = window
